b5614e99a7d0437a44cdf01546b63323ce790569
Rushna Nizami <rushnaulaziz@gmail.com>
2022-02-04 14:49:24 +0500

    Adds item class
    Adds required properties in item class
    
    defence: int
    equipped: bool = False
    name: str
    priority: int
    tile: tuple
    attack: int

---
+++ item.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
+class Item:
+    name: str
+    priority: int
+    tile: tuple
+    attack: int
+    defence: int
+    equipped: bool = False
+    
+    
